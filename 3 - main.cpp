e7b0afd4bdabcfb0c3e3b629cc1fcc43366c85f6
nathaliamoreno <41310257+nathaliamoreno@users.noreply.github.com>
2018-08-10 00:19:37 -0300

    Update 3 - main.cpp

--- 3 - main.cpp
+++ 3 - main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
-#include "Invoice.h"
+#include "Empregado.h"
 #include <string>
 
 using namespace std;
 
 int main()
 {
-        Empregado empregado = Empregado("Nathalia", "Moreno", 3000.0);
+        Empregado empregado1 = Empregado("Nathalia", "Moreno", 2000);
+        Empregado empregado2 = Empregado("Derzu", "Omaia", 12000);
 
-        cout << empregado.salario() <<endl;
+
+        cout << "Nome: " << empregado1.getNome() << " " << empregado1.getSobrenome()<< endl;
+        cout << "Salario Mensal: R$ " << empregado1.getSalario() << endl;
+        cout << "Valor do Salario Anual: R$ " << empregado1.SalarioAnual() << endl;
+        cout << "Aumento de 10% ao mes: R$ " << empregado1.CalculaAumento() << endl;
+        cout << "Valor Anual com Aumento: R$ " << empregado1.CalculaAumento()*12 + empregado1.SalarioAnual() << endl;
+        cout <<"--------------------------------------" << endl;
+        cout << "Nome: " << empregado2.getNome() << " " << empregado2.getSobrenome()<< endl;
+        cout << "Salario Mensal: R$ " << empregado2.getSalario() << endl;
+        cout << "Valor do Salario Anual: R$ " << empregado2.SalarioAnual() << endl;
+        cout << "Aumento de 10% ao mes: R$ " << empregado2.CalculaAumento() << endl;
+        cout << "Valor Anual com Aumento: R$ " << empregado2.CalculaAumento()*12 + empregado2.SalarioAnual() << endl;
+        cout <<"--------------------------------------" << endl;
 
 
     return 0;
